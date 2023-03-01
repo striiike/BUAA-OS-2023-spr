c0e6c2ce6d6256d764d69c9dc2b2e0c0098fb878
hiccup <hhhhiccup@gmail.com>
2023-03-01 21:14:02 +0800

    0.3.1

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -2,5 +2,6 @@
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
 
-awk '/int/ {print FNR}' file
+awk -v pat=$2 '$0~pat {print FNR}' $1 > $3
+
 
